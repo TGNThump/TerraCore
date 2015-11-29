7adcddc233a41f5228e81477fb3774578aad1894
Ben Pilgrim <ben@pilgrim.me.uk>
2015-11-29 23:13:15 +0000

    Initial Commit for Sponge Version

---
+++ src/main/java/uk/co/terragaming/TerraCraft/TerraCraft.java
@@ -0,0 +1,8 @@
+package uk.co.terragaming.TerraCraft;
+
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(id = "terracraft", name = "TerraCraft", version = "0.0.4")
+public class TerraCraft {
+	
+}
